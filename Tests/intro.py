ce5149b7d0273831c9899da44a29ce840ee580af
Diego Ornelas <DiegoOrnelas@Diegos-MBP.home>
2020-04-21 02:40:19 -0400

    Moved files around

---
+++ Tests/intro.py
@@ -0,0 +1,8 @@
+#This is a test file
+#Shift F5 to run on mac
+
+def func():
+	x = "This is a command line test"
+	print x
+
+x = func()
---
+++ Tests/intro2.py
@@ -0,0 +1,5 @@
+def func():
+    x = "This is a test"
+    print x
+
+func()
